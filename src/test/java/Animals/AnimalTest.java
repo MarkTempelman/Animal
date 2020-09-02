86bc892d3411561b2b7435752931b9471e9daeb5
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-09-02 15:20:39 +0200

    Commit

---
+++ src/test/java/Animals/AnimalTest.java
@@ -0,0 +1,33 @@
+package Animals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+    private Animal animal;
+
+    @BeforeEach
+    void setUp() {
+        this.animal = new Animal("Ugly Duckling", Gender.Male);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Ugly Duckling", this.animal.getName());
+        assertEquals(Gender.Male, this.animal.getGender());
+        assertNull(this.animal.getReservedBy());
+    }
+
+    @Test
+    void testReservation(){
+        assertNull(this.animal.getReservedBy());
+        assertTrue(this.animal.reserve("John Doe"));
+        assertNotNull(this.animal.getReservedBy());
+        assertEquals("John Doe", this.animal.getReservedBy().getName());
+        assertFalse(this.animal.reserve("Jane Doe"));
+    }
+
+    //commit
+}
