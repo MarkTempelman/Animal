a5870644395b4fbc9ba5e440cdaf923c99eda2cc
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-09-26 11:05:19 +0200

    Deleted everything again

--- backEnd/src/main/java/Animals/TempTestClass.java
+++
@@ -1,9 +0,0 @@
-package Animals;
-
-import java.text.MessageFormat;
-
-public class TempTestClass {
-    public String greet(String name){
-        return MessageFormat.format("Hello {0}", name);
-    }
-}
--- backEnd/src/main/java/module-info.java
+++
@@ -1,3 +0,0 @@
-module backEnd {
-    exports Animals;
-}
